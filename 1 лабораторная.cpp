db5d516eb8659d3af0724fc724c46bbcfbd4b70e
Tisha1122 <89823333+Tisha1122@users.noreply.github.com>
2021-12-25 13:38:47 +0300

    Add files via upload

---
+++ 1 лабораторная.cpp
@@ -0,0 +1,50 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+/// <Работу выполнил>
+/// Саламатин Тихон Михайлович
+/// <Группа>
+/// <M3111>
+
+int main()
+{
+    // Установка Русского языка
+    setlocale(LC_ALL, "Russian");
+    char oper;
+    double a, b, res;
+    cout << "Выберите операцию:\n";
+    cout << "(+, -, *, /) \n";
+    cin >> oper;
+    switch (oper)
+    {
+    
+    case '+':
+            cout << "Введите данные \n";
+            cin >> a >> b;
+            res = a + b;
+            break; 
+    case '-':
+            cout << "Введите данные \n";
+            cin >> a >> b;
+            res = a - b;
+            break; 
+   
+    case '*':
+            cout << "Введите данные \n";
+            cin >> a >> b;
+            res = a * b;
+            break; 
+    case '/':
+            cout << "Введите данные \n";
+            cin >> a >> b;
+            res = a / b;
+            break; 
+    defoult: 
+            return 0;
+    }
+    
+    cout << "Ответ = " << res;
+}
+
